8a77144ca1d811d7f9b0e09995c237d239e2bed1
Maria2323 <manehka_ilu@mail.ru>
2020-06-05 12:18:42 +0300

    Добавляет функция для создания случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,79 +1,88 @@
 'use strict';
 
-var avatars = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-var types = ['palace', 'flat', 'house', 'bungalo'];
-var checkinCheckout = ['12:00', '13:00', '14:00'];
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var objects = [];
-
-var randomValue = [Math.round(Math.random() * 10)];
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 var getRandomElement = function (characteristic) {
   return characteristic[Math.round(Math.random() * (characteristic.length - 1))];
 };
 
+var randomValue = Math.round(Math.random() * 10);
+
+var createArray = function (number) {
+  var finalArray = [];
+  for (var i = 0; i < (number + 1); i++) {
+    var randomNumeral = Math.round(Math.random() * 10);
+    finalArray.push(randomNumeral);
+  }
+  return finalArray;
+};
+
 var createTitle = function () {
-  return 'Предложение ' + randomValue;
+  return 'Предложение ' + createArray(randomValue)[i];
 };
 
 var createAddress = function () {
-  var x = 100 * randomValue;
-  var y = 50 * randomValue;
+  var x = 100 * createArray(randomValue)[i];
+  var y = 50 * createArray(randomValue)[i];
   return '{' + x + ', ' + y + '}';
 };
 
 var createPrice = function () {
-  var price = 500 * randomValue;
+  var price = 500 * createArray(randomValue)[i];
   return price + 'руб./сутки';
 };
 
-var createRooms = function () {
-  return 1 * randomValue;
-};
-
-var createGuests = function () {
-  return 1 * randomValue;
+var createCharacteristic = function (array) {
+  var resultArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (Math.random() > 0.5) {
+      resultArray.push(array[i]);
+    }
+  }
+  return resultArray;
 };
 
 var createDescription = function () {
-  return 'Описание ' + randomValue;
+  return 'Описание ' + createArray(randomValue)[i];
 };
 
-var createX = function () {
-  return 100 * randomValue;
-};
-
-var createY = function () {
-  return 100 * randomValue;
+var createXY = function () {
+  return 100 * createArray(randomValue)[i];
 };
 
 var createObject = function () {
   return {
     author: {
-      avatar: getRandomElement(avatars)
+      avatar: getRandomElement(AVATARS)
     },
     offer: {
       title: createTitle(),
       address: createAddress(),
       price: createPrice(),
-      type: getRandomElement(types),
-      rooms: createRooms(),
-      guests: createGuests(),
-      checkin: getRandomElement(checkinCheckout),
-      checkout: getRandomElement(checkinCheckout),
-      features: getRandomElement(features),
+      type: getRandomElement(TYPES),
+      rooms: createArray(randomValue)[i],
+      guests: createArray(randomValue)[i],
+      checkin: getRandomElement(CHECKIN_CHECKOUT),
+      checkout: getRandomElement(CHECKIN_CHECKOUT),
+      features: createCharacteristic(FEATURES),
       description: createDescription(),
-      photos: getRandomElement(photos)
+      photos: createCharacteristic(PHOTOS)
     },
     location: {
-      x: createX(),
-      y: createY(),
+      x: createXY(),
+      y: createXY(),
     }
   };
 };
 
+var objects = [];
 for (var i = 0; i < 8; i++) {
   objects.push(createObject());
 }
