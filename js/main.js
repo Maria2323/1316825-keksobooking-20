adbbcb7e974c6e773a7f7f16d1a4cfce97ed02ec
Maria2323 <manehka_ilu@mail.ru>
2020-06-18 09:28:38 +0300

    Добавляет закрытие попапов объявлений и соответствие чекина и чекаута

--- js/main.js
+++ js/main.js
@@ -21,11 +21,14 @@ var selectType = adForm.querySelector('#type');
 var selectRoomNumber = adForm.querySelector('#room_number');
 var selectGuestsNumber = adForm.querySelector('#capacity');
 var buttonSubmit = adForm.querySelector('.ad-form__submit');
+var timeInSelect = adForm.querySelector('#timein');
+var timeOutSelect = adForm.querySelector('#timeout');
 
 var activatePage = function () {
   map.classList.remove('map--faded');
   mapFilters.classList.remove('ad-form--disabled');
   adForm.classList.remove('ad-form--disabled');
+  inputAddress.disabled = true;
   inputAddress.value = (570 + MAIN_PIN_WIDTH * 0.5) + ', ' + (375 + MAIN_PIN_HEIGHT + 5);
   for (var i = 0; i < formFieldsets.length; i++) {
     formFieldsets[i].disabled = false;
@@ -41,6 +44,7 @@ var deactivatePage = function () {
   for (var i = 0; i < formFieldsets.length; i++) {
     formFieldsets[i].disabled = true;
   }
+  inputAddress.disabled = true;
   inputAddress.value = (570 + MAIN_PIN_WIDTH * 0.5) + ',' + (375 + MAIN_PIN_HEIGHT * 0.5);
 };
 
@@ -107,6 +111,28 @@ selectRoomNumber.addEventListener('change', function () {
     selectGuestsNumber[1].disabled = true;
     selectGuestsNumber[2].disabled = true;
     selectGuestsNumber[3].disabled = false;
+  }
+});
+
+timeOutSelect[1].disabled = true;
+timeOutSelect[2].disabled = true;
+
+timeInSelect.addEventListener('change', function () {
+  if (timeInSelect[0].selected === true) {
+    timeOutSelect[0].selected = true;
+    timeOutSelect[1].disabled = true;
+    timeOutSelect[2].disabled = true;
+    timeOutSelect[0].disabled = false;
+  }
+  if (timeInSelect[1].selected === true) {
+    timeOutSelect[1].selected = true;
+    timeOutSelect[1].disabled = false;
+    timeOutSelect[0].disabled = true;
+  }
+  if (timeInSelect[2].selected === true) {
+    timeOutSelect[2].selected = true;
+    timeOutSelect[2].disabled = false;
+    timeOutSelect[1].disabled = true;
   }
 });
 
@@ -183,6 +209,10 @@ var renderPin = function (object) {
   pinElement.querySelector('img').src = object.author.avatar;
   pinElement.querySelector('img').alt = object.offer.title;
   var openPopup = function () {
+    var popup = map.querySelector('.popup');
+    if (popup) {
+      popup.remove();
+    }
     renderAdCard(object);
   };
   pinElement.addEventListener('click', openPopup);
@@ -199,20 +229,27 @@ var addObjects = function (list) {
 
 var adTemplate = document.querySelector('#card').content;
 
+var type = {
+  'flat': 'Квартира',
+  'bungalo': 'Бунгало',
+  'house': 'Дом',
+  'palace': 'Дворец'
+};
+
+var features = function (object, feature, element) {
+  if (!object.offer.features.includes(feature)) {
+    element.querySelector('.popup__feature--' + feature).classList.add('hidden');
+  } else {
+    element.querySelector('.popup__feature--' + feature).classList.remove('hidden');
+  }
+};
+
 var renderAdCard = function (obj) {
   var adCard = adTemplate.cloneNode(true);
   adCard.querySelector('.popup__title').textContent = obj.offer.title;
   adCard.querySelector('.popup__text--address').textContent = obj.offer.address;
   adCard.querySelector('.popup__text--price').textContent = obj.offer.price;
-  if (obj.offer.type === 'flat') {
-    adCard.querySelector('.popup__type').textContent = 'Квартира';
-  } else if (obj.offer.type === 'bungalo') {
-    adCard.querySelector('.popup__type').textContent = 'Бунгало';
-  } else if (obj.offer.type === 'house') {
-    adCard.querySelector('.popup__type').textContent = 'Дом';
-  } else if (obj.offer.type === 'palace') {
-    adCard.querySelector('.popup__type').textContent = 'Дворец';
-  }
+  adCard.querySelector('.popup__type').textContent = type[obj.offer.type];
   adCard.querySelector('.popup__text--capacity').textContent = obj.offer.rooms + ' комнаты для ' + obj.offer.guests + ' гостей';
   adCard.querySelector('.popup__text--time').textContent = 'Заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout;
   adCard.querySelector('.popup__feature--wifi').textContent = 'wifi';
@@ -221,35 +258,8 @@ var renderAdCard = function (obj) {
   adCard.querySelector('.popup__feature--washer').textContent = 'washer';
   adCard.querySelector('.popup__feature--elevator').textContent = 'elevator';
   adCard.querySelector('.popup__feature--conditioner').textContent = 'conditioner';
-  if (!obj.offer.features.includes('wifi')) {
-    adCard.querySelector('.popup__feature--wifi').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--wifi').classList.remove('hidden');
-  }
-  if (!obj.offer.features.includes('dishwasher')) {
-    adCard.querySelector('.popup__feature--dishwasher').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--dishwasher').classList.remove('hidden');
-  }
-  if (!obj.offer.features.includes('parking')) {
-    adCard.querySelector('.popup__feature--parking').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--parking').classList.remove('hidden');
-  }
-  if (!obj.offer.features.includes('washer')) {
-    adCard.querySelector('.popup__feature--washer').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--washer').classList.remove('hidden');
-  }
-  if (!obj.offer.features.includes('elevator')) {
-    adCard.querySelector('.popup__feature--elevator').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--elevator').classList.remove('hidden');
-  }
-  if (!obj.offer.features.includes('conditioner')) {
-    adCard.querySelector('.popup__feature--conditioner').classList.add('hidden');
-  } else {
-    adCard.querySelector('.popup__feature--conditioner').classList.remove('hidden');
+  for (var feat = 0; feat < FEATURES.length; feat++) {
+    features(obj, FEATURES[feat], adCard);
   }
   adCard.querySelector('.popup__description').textContent = obj.offer.description;
   var images = adCard.querySelector('.popup__photos');
@@ -261,15 +271,23 @@ var renderAdCard = function (obj) {
   }
   adCard.querySelector('.popup__avatar').src = obj.author.avatar;
   var mapFiltersContainer = document.querySelector('.map__filters-container');
+  var closeCardBtn = adCard.querySelector('.popup__close');
+  var closeCard = function () {
+    var popup = map.querySelector('.popup');
+    popup.remove();
+    closeCardBtn.removeEventListener('click', CloseCardBtnClick);
+    closeCardBtn.removeEventListener('keydown', closeCardEsc);
+  };
+  var CloseCardBtnClick = function () {
+    closeCard();
+  };
+  var closeCardEsc = function (evt) {
+    if (evt.key === 'Escape') {
+      closeCard();
+    }
+  };
+  closeCardBtn.addEventListener('click', CloseCardBtnClick);
+  document.addEventListener('keydown', closeCardEsc);
   map.insertBefore(adCard, mapFiltersContainer);
-  var closeAdBtn = adCard.querySelector('.popup__close');
-  var closeAd = function () {
-    adCard.remove();
-    closeAdBtn.removeEventListener('click', onCloseAdBtnClick);
-  };
-  var onCloseAdBtnClick = function () {
-    closeAd();
-  };
-  closeAdBtn.addEventListener('click', onCloseAdBtnClick);
   return adCard;
 };
